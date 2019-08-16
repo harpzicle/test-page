bc54540eced24badcdf3231906c48bd925e43dee
benzene <mriklojn@gmail.com>
2019-08-16 09:31:25 +0100

    Update index.html

--- copy.js
+++ copy.js
@@ -1,4 +1,25 @@
-function copy(field) {
-    document.getElementById("reg-output-"+field).select();
-    document.execCommand("copy");
+function clearSelection() { /* stolen from https://stackoverflow.com/questions/6562727/is-there-a-function-to-deselect-all-text-using-javascript */
+    var sel;
+    if ( (sel = document.selection) && sel.empty ) {
+        sel.empty();
+    } else {
+        if (window.getSelection) {
+            window.getSelection().removeAllRanges();
+        }
+        var activeEl = document.activeElement;
+        if (activeEl) {
+            var tagName = activeEl.nodeName.toLowerCase();
+            if ( tagName == "textarea" ||
+                    (tagName == "input" && activeEl.type == "text") ) {
+                // Collapse the selection to the end
+                activeEl.selectionStart = activeEl.selectionEnd;
+            }
+        }
+    }
+}
+
+function copy() {
+    document.activeElement.select();
+    document.execCommand('copy');
+    clearSelection();
 }
