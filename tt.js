265371a0bb0fdfb3f18512ce97b91bb8c355f921
benzene <mriklojn@gmail.com>
2019-11-23 11:42:16 +0000

    Added timer, score and restart function

--- tt.js
+++ tt.js
@@ -2,39 +2,74 @@ var html_a = document.getElementById("a")
 var html_b = document.getElementById("b")
 var html_c = document.getElementById("c")
 var html_gratz = document.getElementById("gratz");
+var html_wrong = document.getElementById("wrong");
+
+var original = "<span id=\"a\">0</span> &times; <span id=\"b\">0</span> = <span id=\"c\"></span>";
+
+var timer_interval = setInterval(timer, 20);
 
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-var a, b, c, score=0, time=Date.now(), limit=10;
+function timer() {
+    var length = (Date.now() - time) / 1000; /* ms to second */
+    html_gratz.innerHTML = `You've got ${score}/${count} questions in ${length.toFixed(1)} seconds.`;
+}
+
+var a=0, b=0, c, score=0, time=Date.now(), limit=5, count = 0;
+var old_a=0, old_b=0;
 var play = true;
 
+function pair_equal(a,b,c,d) {
+    return (a==c && b==d) || (a==d && b==c);
+}
+
 function reset() {
-    a = random(2, 12);
-    b = random(2, 12);
+    while (pair_equal(old_a, old_b, a, b)) {
+        a = random(2, 12);
+        b = random(2, 12);
+    }
+    old_a = a;
+    old_b = b;
     c = "";
     html_a.innerHTML = a;
     html_b.innerHTML = b;
     html_c.innerHTML = c;
 }
 
+function restart() {
+    a=b=c=score=count=old_a=old_b=0;
+    play = true;
+    time=Date.now();
+    document.getElementById("tt").style.display = "block";
+    timer_interval = setInterval(timer, 20);
+}
+
 document.onkeydown = function(e) {
-    if (!play) return;
     if (e.key == "Backspace") {
         e.preventDefault();
         c = c.slice(0, -1);
     }
+    else if (e.key == "Enter") {
+        if (play) {
+            count++;
+            reset();
+        }
+        else restart();
+    }
     else c += e.key;
     if (a*b == c) {
-        score += 1;
+        score++;
+        count++;
         reset();
     }
-    if (score == limit) {
+    if (count == limit) {
         var length = Date.now() - time;
         length /= 1000;
-        document.getElementById("tt").innerHTML = "";
-        html_gratz.innerHTML = `You completed ${score} questions in ${length.toFixed(1)} seconds.`;
+        document.getElementById("tt").style.display = "none";
+        clearInterval(timer_interval);
+        timer();
         play = false;
     }
     html_c.innerHTML = c;
